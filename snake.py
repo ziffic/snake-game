4c87962b47dda447b236dc0c6771d0858aebb6a7
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-13 14:51:22 -0700

    add snaked class

---
+++ snake.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.penup()
+            new_segment.color("white")
+            new_segment.goto(position)
+            new_segment.penup()
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seq_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seq_num - 1].xcor()
+            new_y = self.segments[seq_num - 1].ycor()
+            self.segments[seq_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
