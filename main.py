5d845af3de2751c4712f7d3b82d7270ae694dc87
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-14 14:33:28 -0700

    added high score

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    scoreboard.display_score()
     time.sleep(0.1)
     snake.move()
 
@@ -33,12 +32,12 @@ while game_is_on:
         scoreboard.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -7,20 +7,23 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
-        self.display_score()
+        self.update_scoreboard()
 
-    def display_score(self):
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
-        self.score = self.score + 1
-        self.clear()
-        self.display_score()
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+        self.score += 1
+        self.update_scoreboard()
